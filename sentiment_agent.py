c250cecfd877003e27e95b9225a1ecf5b948924e
3078024889 <150344612+3078024889@users.noreply.github.com>
2026-03-04 20:46:51 +0800

    Delete sentiment_agent.py

--- sentiment_agent.py
+++
@@ -1,8 +0,0 @@
-import random
-
-class SentimentAgent:
-    def analyze_sentiment(self):
-        sentiment = random.choice(["Bullish", "Neutral", "Bearish"])
-        return {
-            "sentiment": sentiment
-        }
